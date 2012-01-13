edacf58b19a3cb24feefd03aa672616388a68877
D. Dotsenko <ddotsenko@willow-systems.com>
2012-01-13 11:16:20 -0800

    Tweaking lisence presentation (for minification)

--- rrdfj.js
+++ rrdfj.js
@@ -1,6 +1,5 @@
-/* @license
-Copyright (c) 2012 WIllow Systems Corporation (www DOT willow-systems DOT com)
-
+/** @preserve Copyright (c) 2012 WIllow Systems Corporation (www.willow-systems.com) ("MIT License") */
+/*
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.

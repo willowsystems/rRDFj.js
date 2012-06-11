5dd13d1c67ac21448cd683033f1a172b33745f96
D. Dotsenko <ddotsenko@willow-systems.com>
2012-06-11 16:16:05 -0700

    Expanding license blob

--- rrdfj.js
+++ rrdfj.js
@@ -1,5 +1,9 @@
-/** @preserve Copyright (c) 2012 WIllow Systems Corporation (www.willow-systems.com) ("MIT License") */
-/*
+/** @preserve rRDFj.js - Reduced RDF as JSON library
+Copyright (c) 2012 WIllow Systems Corporation (www.willow-systems.com)
+"MIT" License
+https://github.com/willowsystems/rRDFj.js
+*/
+/* @license
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
